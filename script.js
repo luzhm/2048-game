3bef9b46511eb30152a9e806e123d83838f5d210
Мария Лужецкая <maryam.lug@mail.ru>
2025-11-30 13:48:34 +0300

    добавлние всех файлов

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('2048 app — script loaded');
+const h = document.createElement('h1');
+h.textContent = '2048 — загружается...';
+h.style.textAlign = 'center';
+h.style.padding = '20px';
+document.body.prepend(h);
