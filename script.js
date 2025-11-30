3692eb3d27500eee44407ffb0fe2ce4af583b2e7
Мария Лужецкая <maryam.lug@mail.ru>
2025-11-30 14:07:03 +0300

    счет

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ app.style.padding = "20px";
 document.body.appendChild(app);
 
 console.log("#app создан");
+const topPanel = document.createElement("div");
+topPanel.id = "top-panel";
+topPanel.style.display = "flex";
+topPanel.style.justifyContent = "space-between";
+topPanel.style.alignItems = "center";
+topPanel.style.marginBottom = "20px";
+
+app.appendChild(topPanel);
+const title = document.createElement("h2");
+title.textContent = "2048";
+title.style.margin = "0";
+title.style.fontSize = "32px";
+title.style.fontWeight = "bold";
+topPanel.appendChild(title);
+
+const scoreBox = document.createElement("div");
+scoreBox.id = "score-box";
+scoreBox.style.textAlign = "right";
+
+scoreBox.innerHTML = `
+  <div id="score" style="font-size: 20px; font-weight: bold;">Счёт: 0</div>
+  <div id="best" style="font-size: 14px; color: #666;">Рекорд: 0</div>
+`;
+
+topPanel.appendChild(scoreBox);
+
+console.log("Заголовок и счет добавлены");
